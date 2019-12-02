ba4fdb176e592c1e1a71c865c1d51b4deb48bce0
Ravi Singh <ravinder.fbd@outlook.com>
2019-12-01 18:24:50 -0800

    num --> num1

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,15 +1,15 @@
 public class HelloJava{
 
-public static String square(int num){
+public static String square(int num1){
 
-return String.valueOf("Square is="+num * num);
+return String.valueOf("Square is="+num1 * num1);
 
 }
 
 
 public static void main(String[] args){
-int num = 2;
-System.out.println(square(num));
+int num1 = 4;
+System.out.println(square(num1));
 
 }
 
