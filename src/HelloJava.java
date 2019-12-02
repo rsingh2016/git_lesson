189e239df36907be3b55f70a4552c9e4b0251247
Ravi Singh <ravinder.fbd@outlook.com>
2019-12-01 18:47:25 -0800

    added sum method

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -8,12 +8,20 @@ return String.valueOf("Square is="+num2 * num2);
 
 
 }
+public static String sum(int num2){
+
+return String.valueOf("sum is="+(num2 + num2));
+
+
+}
+
 
 
 public static void main(String[] args){
 
 int num2 = 10;
 System.out.println(square(num2));
+System.out.println(sum(num2));
 
 
 }
