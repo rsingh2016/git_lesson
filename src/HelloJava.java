59642e807d80c94b4e63777fc43bc6bf63f020fc
Ravi Singh <ravinder.fbd@outlook.com>
2019-12-01 19:01:32 -0800

    added comments to sum method

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -10,7 +10,7 @@ return String.valueOf("Square is="+num2 * num2);
 }
 public static String sum(int num2){
 
-return String.valueOf("sum is="+(num2 + num2));
+return String.valueOf("Sum is="+(num2 + num2));
 
 
 }
