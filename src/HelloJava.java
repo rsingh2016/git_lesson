5e5717b732bff6bea673ee70dfa5fe99f12528e6
Ravi Singh <ravinder.fbd@outlook.com>
2019-12-01 18:08:10 -0800

    added hello.java

---
+++ src/HelloJava.java
@@ -0,0 +1,16 @@
+public class HelloJava{
+
+public static String square(int num){
+
+return String.valueOf(num * num);
+
+}
+
+
+public static void main(String[] args){
+int num = 5;
+System.out.println(square(num));
+
+}
+
+}
